2f2960d1e2c6ed3bd4e0afb089eadb2e2e6c4b6b
gaurav <gauravsahadev8409@gmail.com>
2020-03-12 16:42:34 +0530

    text animation

--- js/text.js
+++ js/text.js
@@ -1,21 +1,56 @@
-var words = ['Optimizer', 'Analyzer', 'Debugger', 'Builder', 'Developer'];
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
-textSequence(0);
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-function textSequence(i) {
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
 
-    if (words.length > i) {
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-        setTimeout(function () {
-            document.getElementById("sequence").innerHTML = words[i]
-            textSequence(++i);
+  var that = this;
+  var delta = 300 - Math.random() * 100;
 
-        }, 1000); // 1 second (in milliseconds)
+  if (this.isDeleting) { delta /= 2; }
 
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
 
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
 
-    } else if (words.length == i) { // Loop
-        textSequence(0);
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
-
-}
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
